11be76f9be3b3eabc3c341259e2a4682d3df862e
vo5an <114090483+vo5an@users.noreply.github.com>
2023-01-03 12:24:04 +0700

    Add files via upload
    Программа для поиска палиндром в файле 2of12inf.txt.

---
+++ palindromes.py
@@ -0,0 +1,14 @@
+"""Отыскивает палиндромы (буквенные балинграммы) в файле словаря."""
+
+import load_dictionary
+
+word_list = load_dictionary.load('2of12inf.txt')
+pali_list = []
+
+for word in word_list:
+    if len(word) > 1 and word == word[::-1]:
+        pali_list.append(word)
+
+print("\nЧисло обнаруженных палиндромов = {}\n".format(len(pali_list)))
+
+print(*pali_list, sep='\n')
